9369bb9ccafb9f175e305a5c5bdbc7f4deab269d
seongck <seongk@pm.me>
2019-09-29 20:29:33 -0700

    Set up App component

---
+++ app/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Component
+// State
+// Lifecycle
+// UI
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  // React Element,
+  <App />,
+  // Where to render the Element to
+  document.getElementById('app');
+)
