f405052e85bd7628e2852d95f590e496e5814a3c
seongck <seongk@pm.me>
2019-10-26 18:28:18 -0700

    Fix typo on line 69

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -66,7 +66,7 @@ class PlayerInput extends React.Component {
             onChange={this.handleChange}
           />
           <button 
-            className='btn btn-dark' 
+            className='btn dark-btn' 
             type='submit' 
             disabled={!this.state.username}
           >
