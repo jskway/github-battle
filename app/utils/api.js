71ac497f6e2087905007b65eef80a3e02aeba923
seongck <seongk@pm.me>
2019-10-27 19:43:37 -0700

    Add filler id, secret and params and adjust fetch URLs

--- app/utils/api.js
+++ app/utils/api.js
@@ -1,60 +1,66 @@
-function getErrorMsg(message, username) {
+const id = "YOUR_CLIENT_ID"
+const sec = "YOUR_SECRET_ID"
+const params = `?client_id=${id}&client_secret=${sec}`
+
+function getErrorMsg (message, username) {
   if (message === 'Not Found') {
-    return `${username} doesn't exist`;
+    return `${username} doesn't exist`
   }
 
-  return message;
+  return message
 }
 
-function getProfile(username) {
-  return fetch(`https://api.github.com/users/${username}`)
-    .then( res => res.json())
-    .then( profile => {
-      if(profile.message) {
-        throw new Error(getErrorMsg(profile.message, username));
+function getProfile (username) {
+  return fetch(`https://api.github.com/users/${username}${params}`)
+    .then((res) => res.json())
+    .then((profile) => {
+      if (profile.message) {
+        throw new Error(getErrorMsg(profile.message, username))
       }
-      return profile;
+
+      return profile
     })
 }
 
-function getRepos(username) {
-  return fetch(`https://api.github.com/users/${username}/repos&per_page=100`)
-    .then( res => res.json())
-    .then( repos => {
-      if(profile.message) {
-        throw new Error(getErrorMsg(repos.message, username));
+function getRepos (username) {
+  return fetch(`https://api.github.com/users/${username}/repos${params}&per_page=100`)
+    .then((res) => res.json())
+    .then((repos) => {
+      if (repos.message) {
+        throw new Error(getErrorMsg(repos.message, username))
       }
-      return repos;
+
+      return repos
     })
 }
 
-function getStarCount(repos) {
-  repos.reduce((count, { stargazers_count }) => count + stargazers_count, 0)
+function getStarCount (repos) {
+  return repos.reduce((count, { stargazers_count }) => count + stargazers_count , 0)
 }
 
-function calculateScore(followers, repos) {
-  return (followers * 3) + getStarCount(repos) 
+function calculateScore (followers, repos) {
+  return (followers * 3) + getStarCount(repos)
 }
 
-function getUserData(player) {
+function getUserData (player) {
   return Promise.all([
     getProfile(player),
     getRepos(player)
-  ]).then(([ profile, repos]) => ({
+  ]).then(([ profile, repos ]) => ({
     profile,
     score: calculateScore(profile.followers, repos)
   }))
 }
 
-function sortPlayers(players) {
-  return players.sort((a, b) => b.score - a.score);
+function sortPlayers (players) {
+  return players.sort((a, b) => b.score - a.score)
 }
 
-export function battle(players) {
+export function battle (players) {
   return Promise.all([
     getUserData(players[0]),
     getUserData(players[1])
-  ]).then( results => sortPlayers(results))
+  ]).then((results) => sortPlayers(results))
 }
 
 export function fetchPopularRepos(language) {
