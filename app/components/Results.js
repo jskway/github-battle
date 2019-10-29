550e3d88b93e0cc0ecbc0171039c45ceafc03e6a
seongck <seongk@pm.me>
2019-10-28 21:28:46 -0700

    Refactor Results component to render Card with children

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { battle } from '../utils/api';
 import { FaCompass, FaBriefcase, FaUsers, FaUserFriends, FaCode, FaUser } from 'react-icons/fa';
+import Card from './Card';
 
 export default class Results extends React.Component {
   constructor(props) {
@@ -48,23 +49,13 @@ export default class Results extends React.Component {
 
     return (
       <div className='grid space-around container-sm'>
-        <div className='card bg-light'>
-          <h4 className='header-lg center-text'>
-            { winner.score === loser.score ? 'Tie' : 'Winner' }
-          </h4>
-          <img
-            className='avatar'
-            src={winner.profile.avatar_url}
-            alt={`Avatar for ${winner.profile.login}`}
-          />
-          <h4 className='center-text'>
-            Score: {winner.score.toLocaleString()}
-          </h4>
-          <h2 className='center-text'>
-            <a className='link' href={winner.profile.html_url}>
-              {winner.profile.login}
-            </a>
-          </h2>
+        <Card 
+          header={ winner.score === loser.score ? 'Tie' : 'Winner' }
+          subheader={`Score: ${winner.score.toLocaleString()}`}
+          avatar={winner.profile.avatar_url}
+          href={winner.profile.html_url}
+          name={winner.profile.login}
+        >
           <ul className='card-list'>
             <li>
               <FaUser color='rgb(239, 115, 115)' size={22} />
@@ -91,8 +82,7 @@ export default class Results extends React.Component {
               { winner.profile.following.toLocaleString() } following 
             </li>
           </ul>
-        </div>
-
+        </Card>
         <div className='card bg-light'>
           <h4 className='header-lg center-text'>
             { winner.score === loser.score ? 'Tie' : 'Loser' }
