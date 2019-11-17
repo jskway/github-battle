75553be590ecbb59dd07771c27b4a4df7a997b7c
seongck <seongk@pm.me>
2019-11-16 21:26:20 -0800

    Refactor to use render props pattern instead of HOC

--- app/components/Tooltip.js
+++ app/components/Tooltip.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import withHover from './withHover';
+import Hover from './Hover';
 
 const styles = {
   container: {
@@ -24,20 +24,21 @@ const styles = {
   }
 }
 
-function Tooltip({ text, children, hovering }) {
-    return (
-      <div 
-        style={styles.container}>
-          {hovering === true && <div style={styles.tooltip}>{text}</div>}
-          {children}
-      </div>
-    );
-
+export default function Tooltip({ text, children }) {
+  return (
+    <Hover>
+      {(hovering) => (
+        <div 
+          style={styles.container}>
+            {hovering === true && <div style={styles.tooltip}>{text}</div>}
+            {children}
+        </div>
+      )}
+    </Hover>
+  );
 }
 
 Tooltip.propTypes = {
   text: PropTypes.string.isRequired,
-  hovering: PropTypes.bool.isRequired
 }
 
-export default withHover(Tooltip, 'hovering');
