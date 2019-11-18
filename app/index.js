c16d03c3dde4468713d53f57483fa04eb775a802
seongck <seongk@pm.me>
2019-11-17 18:14:48 -0800

    Wrap app in ThemeProvider and add theme state with toggler method

--- app/index.js
+++ app/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Popular from './components/Popular';
 import Battle from './components/Battle';
+import { ThemeProvider } from './contexts/theme';
 
 // Component
 // State
@@ -10,11 +11,25 @@ import Battle from './components/Battle';
 // UI
 
 class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      theme: 'light',
+      toggleTheme: () => {
+        this.setState(({ theme }) => ({
+          theme: theme === 'light' ? 'dark' : 'light'
+        }))
+      }
+    }
+  }
+
   render() {
     return (
-      <div className="container">
-        <Battle />
-      </div>
+      <ThemeProvider value={this.state}>
+        <div className="container">
+          <Battle />
+        </div>
+      </ThemeProvider>
     )
   }
 }
