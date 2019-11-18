0f591cfbe9b32b821ecb5cdebd6ec383d265da43
seongck <seongk@pm.me>
2019-11-17 20:09:20 -0800

    Wrap App UI in Router and add Routes

--- app/index.js
+++ app/index.js
@@ -5,6 +5,7 @@ import Popular from './components/Popular';
 import Battle from './components/Battle';
 import { ThemeProvider } from './contexts/theme';
 import Nav from './components/Nav';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 // Component
 // State
@@ -26,14 +27,17 @@ class App extends React.Component {
 
   render() {
     return (
-      <ThemeProvider value={this.state}>
-        <div className={this.state.theme}>
-          <div className="container">
-            <Nav />
-            <Battle />
+      <Router>
+        <ThemeProvider value={this.state}>
+          <div className={this.state.theme}>
+            <div className="container">
+              <Nav />
+              <Route path='/' exact component={Popular} />
+              <Route path='/battle' component={Battle} />
+            </div>
           </div>
-        </div>
-      </ThemeProvider>
+        </ThemeProvider>
+      </Router>
     )
   }
 }
